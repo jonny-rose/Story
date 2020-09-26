827dc0e7dca48adcfaa36feca875d2edb2caa5c9
Mihai Blebea <mihai@getchip.uk>
2020-09-26 22:12:37 +0100

    Adds route for signup in server

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
 const sql = require("./database.js");
 
 sql.userTable();
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Server works");
 });
 
-
+app.post('/signup', (request,response)=> {
+    console.log(request.body)
+    res.json(request.body);
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
